7c29dd6cef98e7c7b0c40f91f0001deb0850161d
Samweli <smwltwesa6@gmail.com>
2024-06-27 13:27:16 +0300

    plugin source structure

--- docs/src/user/guide/index.md
+++ docs/src/user/guide/index.md
@@ -3,7 +3,7 @@ title: GEA AFFORESTATION
 summary: PROJECT_SUMMARY
     - Ketan Bamniya
 date: 19-06-2024
-some_url: https://github.com/kartoza/gea-qgis-plugin
+some_url: https://github.com/kartoza/qgis-gea-plugin
 copyright: Copyright 2024, PROJECT_OWNER
 contact: PROJECT_CONTACT
 license: This program is free software; you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
---
+++ src/qgis_gea_plugin/gui/qgis_plugin_template.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+ The plugin main window class file
+"""
+
+import os
+
+from qgis.PyQt import (
+    QtCore,
+    QtGui,
+    QtWidgets,
+    QtNetwork,
+)
+from qgis.PyQt.uic import loadUiType
+from qgis.core import (
+    Qgis,
+    QgsCoordinateReferenceSystem,
+    QgsGeometry,
+    QgsRectangle,
+    QgsWkbTypes,
+)
+
+from qgis.gui import (
+    QgsMessageBar,
+    QgsMapCanvas,
+    QgsRubberBand,
+)
+
+from qgis.utils import iface
+
+from ..resources import *
+
+
+WidgetUi, _ = loadUiType(
+    os.path.join(os.path.dirname(__file__), "../ui/qgis_gea_plugin.ui")
+)
+
+
+class QgisGeaPlugin(QtWidgets.QDockWidget, WidgetUi):
+    """Main plugin UI"""
+
+    def __init__(
+        self,
+        iface,
+        parent=None,
+    ):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.iface = iface
